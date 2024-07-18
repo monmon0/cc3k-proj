3d83d90257fdabf12dd54cd79c5ae26995d45b93
Eason Li <liiiiyuxuan@gmail.com>
2024-07-18 18:15:48 -0400

    finally

--- enemies.d
+++ enemies.d
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-enemies.o: enemies.cc Enemies.h asciiart.h Subject.h Players.h \
-  decorator.h
-=======
-Enemies.o: Enemies.cc
->>>>>>> 2587b65e66e9d568f90254e623f57e55cc6ce00b
+Enemies.o: Enemies.cc Enemies.h asciiart.h Subject.h Players.h \
+  decorator.h Potion.h Observer.h
