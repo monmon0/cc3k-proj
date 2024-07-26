fac9968d974fbed4c3633a494c8f7851b589b4c6
sylvia <sylvianiu4@gmail.com>
2024-07-25 23:18:01 -0400

    fixed dragon hoard

--- Dragon_Hoard.cc
+++ Dragon_Hoard.cc
@@ -1,12 +1,11 @@
 #include "Dragon_Hoard.h"
 
 char Dragon_Hoard::charAt(int row, int col) {
-    if (active && isGuarded && col == this->x && row == this->y) {
-        if (next->charAt(row, col) == '@') {
+    if (active && col == this->x && row == this->y) {
+        if (isGuarded && next->charAt(row, col) == '@') {
             return '@';
-        } else {
-            return 'G';
-        }   
+        }
+        return 'G';
     }
     return next->charAt(row, col);
 }
@@ -15,7 +14,7 @@ Dragon_Hoard::Dragon_Hoard(AsciiArt *next, int x, int y): Treasure{next, x, y, 6
                                                                 isGuarded{true}{}
 
 void Dragon_Hoard::applyEffect(Player *player) {
-    if (!isGuarded) {
+    if (!isGuarded && active) {
         player->addGold(getAmt());
         active = false;
         announcement = "PC obtained" + std::to_string(getAmt()) + "gold";
@@ -24,7 +23,6 @@ void Dragon_Hoard::applyEffect(Player *player) {
 
 void Dragon_Hoard::unGuarded() {
     isGuarded = false;
-    active = true; 
 }
 
 bool Dragon_Hoard::collectable() {
